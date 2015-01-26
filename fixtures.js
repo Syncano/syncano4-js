443d42aea7e32a82c39ae07b2fe4f5fe4a308d85
Artur Wróbel <arturwrobel@home.pl>
2015-01-26 16:08:04 +0100

    fixtures - users list

---
+++ fixtures.js
@@ -0,0 +1,41 @@
+var UserFixtures = [{
+	first_name: 'Pan',
+	last_name: 'Kleks',
+	year_of_birth: 1965
+}, {
+	first_name: 'Mis',
+	last_name: 'Kolargol',
+	year_of_birth: 1970
+}, {
+	first_name: 'Alojzy',
+	last_name: 'Bąbel',
+	year_of_birth: 1980
+}, {
+	first_name: 'Franc',
+	last_name: 'Mauer',
+	year_of_birth: 1955
+}, {
+	first_name: 'Pan',
+	last_name: 'Gietek',
+	year_of_birth: 1976
+}, {
+	first_name: 'John',
+	last_name: 'Little',
+	year_of_birth: 2000
+}, {
+	first_name: 'Donald',
+	last_name: 'Dusk',
+	year_of_birth: 1963
+}, {
+	first_name: 'Kim',
+	last_name: 'Smith',
+	year_of_birth: 1936
+}, {
+	first_name: 'Mister',
+	last_name: 'Banks',
+	year_of_birth: 1925
+}, {
+	first_name: 'Andrew',
+	last_name: 'Kay',
+	year_of_birth: 2003
+}];
