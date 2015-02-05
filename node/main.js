816f09f8ad42b3f4db77df2b27175f156311bea4
Artur Wróbel <arturwrobel@home.pl>
2015-02-05 10:51:24 +0100

    node.js example code finished

--- node/main.js
+++ node/main.js
@@ -9,7 +9,7 @@ var Config = {
 	apiKey: 'api-key'
 };
 
-// Config = require('./config');
+Config = require('./config');
 var Syncano = require('../lib/lib.js');
 
 var syncano = new Syncano(Config.instance);
@@ -17,4 +17,11 @@ syncano.connect(Config.email, Config.password).then(function() {
 	console.log('Account key:', syncano.getInfo().account.account_key);
 }, function(err) {
 	console.log('ERROR: ', err);
-});
+}).then(function() {
+	return syncano.Instances.list();
+}).then(function(List) {
+	console.log('Instances found:', List.length);
+	if (List.length > 0) {
+		console.log('First instance details:', List.at(0));
+	}
+})
