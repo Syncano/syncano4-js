9f7163bf3e13946fdabde8fbd3b3f206ceced390
Artur Wróbel <arturwrobel@home.pl>
2015-02-05 10:48:09 +0100

    fixed nodeRequest method

--- node/main.js
+++ node/main.js
@@ -1,12 +1,20 @@
 'use strict';
 
-var request = require('request');
-var Config = require('./config');
+var Config = {
+	instance: 'instance-name',
+	// use email and password
+	email: 'your-email',
+	password: 'your-password',
+	// or apiKey
+	apiKey: 'api-key'
+};
+
+// Config = require('./config');
 var Syncano = require('../lib/lib.js');
 
-var syncano = new Syncano(Config);
-syncano.connect(Config.apiKey).then(function() {
-	console.log(syncano.getInfo().account.account_key);
+var syncano = new Syncano(Config.instance);
+syncano.connect(Config.email, Config.password).then(function() {
+	console.log('Account key:', syncano.getInfo().account.account_key);
 }, function(err) {
 	console.log('ERROR: ', err);
 });
