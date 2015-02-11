967bfcf8bcc4a7569b62485b20cb0b1c2c4e4bb1
Artur Wróbel <arturwrobel@home.pl>
2015-02-11 11:42:16 +0100

    createAccount script now creates both account and instance and returns config object to write to config.js file

--- test/node/createAccount.js
+++ test/node/createAccount.js
@@ -1,13 +1,56 @@
 'use strict';
 
-var Syncano = require('../../lib/syncano4.js');
+/* place your data here */
+var Config = {
+	password: 'your-password',
+	email: 'your-email',
+	firstName: 'first-name',
+	lastName: 'last-name',
+	instance: 'your-instance-name',
+	instance_description: 'instance-description'
+};
 
+
+
+var Syncano = require('../../lib/syncano4.js');
 var syncano = new Syncano();
-syncano.Accounts.create({
-	email: 'your-email@domain.com',
-	first_name: 'Name',
-	last_name: 'LastName',
-	password: 'Password'
-}).then(function(res) {
-	console.log(res);
+
+createAccount().then(function(account) {
+	return authorize();
+}).then(function() {
+	return createInstance();
+}).then(function(instance) {
+	var arr = [
+		"var Config = {",
+		"\tinstance: '" + Config.instance + "',",
+		"\temail: '" + Config.email + "',",
+		"\tpassword: '" + Config.password + "',",
+		"\tapiKey: ''",
+		"};"
+	];
+	console.log('Done.');
+	console.log(arr.join('\n'));
+}, function(err) {
+	console.log('Error', err);
 });
+
+
+function createAccount() {
+	return syncano.Accounts.create({
+		email: Config.email,
+		first_name: Config.firstName,
+		last_name: Config.lastName,
+		password: Config.password
+	});
+}
+
+function authorize() {
+	return syncano.connect(Config.email, Config.password);
+}
+
+function createInstance() {
+	return syncano.Instances.create({
+		name: Config.instance,
+		description: Config.instance_description
+	});
+}
