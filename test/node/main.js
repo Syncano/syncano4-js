6fe2f7c358055cd5d378b965c901205de02b6236
Artur Wróbel <arturwrobel@home.pl>
2015-02-20 11:58:35 +0100

    modified node.js test file

--- test/node/main.js
+++ test/node/main.js
@@ -18,14 +18,14 @@ var Syncano = require('../../lib/syncano4.js');
 
 var syncano = new Syncano(Config.instance);
 syncano.connect(Config.email, Config.password).then(function() {
-	console.log('Account key:', syncano.getInfo().account.account_key);
+	syncano.ApiKeys.list().then(function(List) {
+		console.log('ApiKeys found:', List.length);
+		if (List.length > 0) {
+			console.log('First apiKey details:', List.at(0));
+		}
+	});
 }, function(err) {
 	console.log('ERROR: ', err);
 }).then(function() {
-	return syncano.Instances.list();
-}).then(function(List) {
-	console.log('Instances found:', List.length);
-	if (List.length > 0) {
-		console.log('First instance details:', List.at(0));
-	}
-})
+	// pass your own code here...
+});
